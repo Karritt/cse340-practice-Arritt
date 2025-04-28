ef8a04ba041b5e018babe5d0a534c5389a63da69
Karritt <45860943+Karritt@users.noreply.github.com>
2025-04-28 14:30:40 -0600

    Intro to Routing update

--- server.js
+++ server.js
@@ -1,28 +1,59 @@
-// Import express using ESM syntax
 import express from 'express';
- 
-// Create an instance of an Express application
-const app = express();
- 
-// Define a route handler for the root URL ('/')
-app.get('/', (req, res) => {
-    res.send('Hello, World! <a href="/about">Go to About</a>');
-});
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-app.get('/about', (req, res) => {
-    res.send('About Page <a href="/">Go back</a>');
-});
 
-app.get("/api", (req, res) => {
-    res.json({
-        message: "Hello from the API!" 
+
+// CONSTS ---------------------------------------------------------------
+    // Create __dirname and __filename variables
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    // Define the port number the server will listen on
+    const PORT = process.env.PORT || 3000;
+
+    // Create an instance of an Express application
+    const app = express();
+
+// MIDDLEWARE -----------------------------------------------------------
+    // Static files from public directory
+    app.use(express.static(path.join(__dirname, 'public')));
+
+// ROUTES ---------------------------------------------------------------
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '/src/views/home.html'));
     });
-});
- 
-// Define the port number the server will listen on
-const PORT = process.env.PORT || 3000;
- 
-// Start the server and listen on the specified port
-app.listen(PORT, () => {
-    console.log(`Server is running on http://127.0.0.1:${PORT}`);
-});
+    
+    app.get('/page1', (req, res) => {
+        res.sendFile(path.join(__dirname, '/src/views/page1.html'));
+    });
+    
+    app.get('/page2', (req, res) => {
+        res.sendFile(path.join(__dirname, '/src/views/page2.html'));
+    });
+
+
+    // Define a route handler for the root URL ('/')
+    // app.get('/', (req, res) => {
+    //     res.send('Hello, World! <a href="/about">Go to About</a>');
+    // });
+
+    
+
+    // app.get('/about', (req, res) => {
+    //     res.send('About Page <a href="/">Go back</a>');
+    // });
+
+    // app.get("/api", (req, res) => {
+    //     res.json({
+    //         message: "Hello from the API!" 
+    //     });
+    // });
+
+
+
+//Set the listener ------------------------------------------------------
+    // Start the server and listen on the specified port
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://127.0.0.1:${PORT}`);
+    });
